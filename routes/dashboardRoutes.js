66f1916d58d52e1d98585ca44293a2d9babd9445
“Ruben <rubenfilemon22@gmail.com>
2023-09-19 19:59:09 +0700

    update dashControll

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const dashboardController = require("../controllers/dashboardController");
+const getUserIdMiddleware = require("../middlewares/getUserIdMiddleware.js");
 
-router.get("/", dashboardController.getAllData);
+router.use(getUserIdMiddleware);
+router.get("/", dashboardController.verifyUser, (req, res) => {
+    return res.json({Status: "Success", role: req.role, id: req.id})
+});
+
 
 module.exports = router;
