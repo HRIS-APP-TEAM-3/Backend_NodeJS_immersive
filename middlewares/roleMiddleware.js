31ccc71eb12ced01deac07e8e5fa13b386805191
Anggawan Ridho <anggawanridho@gmail.com>
2023-09-22 11:14:25 +0700

    role dan reimburse

---
+++ middlewares/roleMiddleware.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken");
+
+function roleMiddleware(requiredRole) {
+  const secretKey = `verysectretkey007`;
+  return (req, res, next) => {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    if (!token) {
+      return res.status(401).json({
+        message: "Authentication Failed, token not found",
+      });
+    }
+    jwt.verify(token, secretKey, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({
+          message: "Authentication failed, no valid token found",
+        });
+      }
+
+      if (!requiredRole.includes(decoded.roleId)) {
+        return res.status(401).json({
+          message:
+            "Authentication Failed, role tidak valid, anda tidak diizinkan untuk mengakses ini",
+        });
+      }
+
+      req.user = decoded;
+      console.log(req.user);
+      next();
+    });
+  };
+}
+
+module.exports = roleMiddleware;
--- routes/reimburseRoutes.js
+++ routes/reimburseRoutes.js
@@ -2,13 +2,46 @@ const express = require("express");
 const router = express.Router();
 const reimburseController = require("../controllers/reimburseController");
 const getUserIdMiddleware = require("../middlewares/getUserIdMiddleware.js");
+const roleMiddleware = require("../middlewares/roleMiddleware");
+const allUser = [1, 2, 3, 4];
+// 1, Superadmin
+// 2, Admin
+// 3, Manager
+// 4, Employee
 
 router.use(getUserIdMiddleware("verysectretkey007"));
 
-router.get("/", reimburseController.getAllReimburse);
-router.post("/", reimburseController.addReimburse);
-router.get("/:reimburseIndex", reimburseController.getReimburseById);
-router.put("/:reimburseIndex", reimburseController.updateReimburse);
-router.delete("/:reimburseIndex", reimburseController.deleteReimburse);
+router.get(
+  "/alluser",
+  roleMiddleware([1, 2, 3]),
+  reimburseController.getAllUserReimburse
+);
+router.get(
+  "/approved",
+  roleMiddleware([1, 2, 3]),
+  reimburseController.getApprovedReimburse
+);
+router.get(
+  "/pending",
+  roleMiddleware([1, 2, 3]),
+  reimburseController.getPendingReimburse
+);
+router.get("/", roleMiddleware(allUser), reimburseController.getAllReimburse);
+router.post("/", roleMiddleware(allUser), reimburseController.addReimburse);
+router.get(
+  "/:reimburseIndex",
+  roleMiddleware(allUser),
+  reimburseController.getReimburseById
+);
+router.put(
+  "/:reimburseIndex",
+  roleMiddleware([2, 3]),
+  reimburseController.updateReimburse
+);
+router.delete(
+  "/:reimburseIndex",
+  roleMiddleware([2, 3]),
+  reimburseController.deleteReimburse
+);
 
 module.exports = router;
